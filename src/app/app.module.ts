8573b9801f6612cb8497a45d0bdb7f6a5f370483
Alejandro <alhemoasde@gmail.com>
2021-12-04 00:32:42 -0500

    alhemoasde@gmail.com: Avance frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConsumoComponent } from './consumo/consumo/consumo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsumoComponent
   ],
   imports: [
     BrowserModule,
